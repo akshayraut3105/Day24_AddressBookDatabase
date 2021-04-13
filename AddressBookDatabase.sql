7b1b58e0c2dd2b49a132bc121d8e54c8e55e6593
akshayraut3105 <akshayraut3105@gmail.com>
2021-04-13 14:15:41 +0530

    [AKSHAY] added Display of contact by group

--- AddressBookDatabase.sql
+++ AddressBookDatabase.sql
@@ -1,14 +1,7 @@
---UC 9 : Ability to identify each Address Book with name and Type
+--UC 10 : Ability to get number of contact persons i.e. count by type
 
 use addressbook_service;
 select * from addressbook;
---Altering address_book to add new columns, addressbookName and Type of contacts
-alter table addressbook add AddressBookName varchar(100),ContactType varchar(100);
---Updating the new columns
-update addressbook set AddressBookName='Akash';
-update addressbook set ContactType='FRIENDS' where State_Name='Gujrat';
-update addressbook set ContactType='FAMILY' where State_Name='Tripura';
-update addressbook set ContactType='PROFESSION' where State_Name='Rajasthan';
 
---Find the details with given address book name and given contact type
-select * from addressbook where AddressBookNAme='Akash' and ContactType='FRIENDS';
+--Getting number of contact persons for each ContactType
+select ContactType,Count(ContactType) as NumberOfContacts from addressbook group by ContactType;
