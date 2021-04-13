b3efa802cd9bf737dce2ede28b90d76be4c4c565
akshayraut3105 <akshayraut3105@gmail.com>
2021-04-13 14:10:28 +0530

    [AKSHAY] added method for sorting

--- AddressBookDatabase.sql
+++ AddressBookDatabase.sql
@@ -1,12 +1,8 @@
---UC 7 : Ability to understand the size of address book by City and State
+--UC 8 : Ability to retrieve entries sorted alphabetically by Person’s name for a given city
 
 use addressbook_service;
---Inserting more data into table
-insert into addressbook values
-('Peter','Parker','Sector 32','Surat','Gujrat',137667,7765434567,'peter@gmail.com'),
-('Virat','Kohli','Block 2','Varodra','Gujrat',138675,9876545678,'virat@gmail.com');
+--Displaying table
 select * from addressbook;
---Find size of address book by city
-select City,count(City) as PeopleInCity from addressbook group by City;
---Find size of address book by State
-select State_Name,count(State_Name) as PeopleInState from addressbook group by State_Name;
+--Retrieve entries sorted alphabetically by name for Surat
+select * from addressbook where City='Surat' order by FirstName+LastName;
+
